59b668747a1ca32b8aab9b55b37b9fda3e866d76
Benjamin K. Stuhl <bks@yochanan.(none)>
2008-11-13 21:56:49 -0700

    hook the deflate decoders into the plugin system

--- libqz7/plugins/BuiltinPlugin.cpp
+++ libqz7/plugins/BuiltinPlugin.cpp
@@ -5,6 +5,8 @@
 #include <QtCore/QList>
 #include <QtCore/QStringList>
 
+#include "codecs/deflate/DeflateDecoder.h"
+
 #include "volumes/singlefile/SingleFileVolume.h"
 
 namespace qz7 {
@@ -21,12 +23,18 @@ Archive *BuiltinPlugin::createArchive(const QString& mimeType, Volume *volume) c
 
 QStringList BuiltinPlugin::decoderNames() const
 {
-    return QStringList();
+    return QStringList()
+        << "deflate"
+        << "deflateNSIS"
+        << "deflate64";
 }
 
 QList<int> BuiltinPlugin::decoderIds() const
 {
-    return QList<int>();
+    return QList<int>()
+        << 0x40108
+        << 0x40901
+        << 0x40109;
 }
 
 QStringList BuiltinPlugin::encoderNames() const
@@ -41,12 +49,28 @@ QList<int> BuiltinPlugin::encoderIds() const
 
 Codec *BuiltinPlugin::createDecoder(const QString& name, QObject *parent) const
 {
+    if (name == "deflate")
+        return new deflate::DeflateDecoder(parent);
+    if (name == "deflate64")
+        return new deflate::Deflate64Decoder(parent);
+    if (name == "deflateNSIS")
+        return new deflate::DeflateNSISDecoder(parent);
+
     return 0;
 }
 
 Codec *BuiltinPlugin::createDecoder(int id, QObject *parent) const
 {
-    return 0;
+    switch (id) {
+    case 0x40108:
+        return new deflate::DeflateDecoder(parent);
+    case 0x40109:
+        return new deflate::Deflate64Decoder(parent);
+    case 0x40901:
+        return new deflate::DeflateNSISDecoder(parent);
+    default:
+        return 0;
+    }
 }
 
 Codec *BuiltinPlugin::createEncoder(const QString& name, QObject *parent) const
