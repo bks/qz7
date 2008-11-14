72e71840d1e67b4fbce67d52f0f023e6f0bfaf63
Benjamin K. Stuhl <bks@coldkeck2.colorado.edu>
2008-11-14 12:59:57 -0700

    some whitespace fixes

--- libqz7/plugins/codecs/deflate/DeflateConst.h
+++ libqz7/plugins/codecs/deflate/DeflateConst.h
@@ -43,20 +43,55 @@ enum {
 
 enum { LevelMask = 0xF };
 
-const quint8 LenStart32[FixedLenTableSize] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 20, 24, 28, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 255, 0, 0};
-const quint8 LenStart64[FixedLenTableSize] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 20, 24, 28, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 0, 0, 0};
+const quint8 LenStart32[FixedLenTableSize] = {
+    0, 1, 2, 3, 4, 5, 6, 7, 8,
+    10, 12, 14, 16, 20, 24, 28, 32,
+    40, 48, 56, 64, 80, 96, 112, 128,
+    160, 192, 224, 255, 0, 0
+};
 
-const quint8 LenDirectBits32[FixedLenTableSize] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4,  4,  5,  5,  5,  5, 0, 0, 0};
-const quint8 LenDirectBits64[FixedLenTableSize] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4,  4,  5,  5,  5,  5, 16, 0, 0};
+const quint8 LenStart64[FixedLenTableSize] = {
+    0, 1, 2, 3, 4, 5, 6, 7, 8,
+    10, 12, 14, 16, 20, 24, 28, 32,
+    40, 48, 56, 64, 80, 96, 112, 128,
+    160, 192, 224, 0, 0, 0
+};
 
-const quint32 DistStart[DistTableSize64]  = {0, 1, 2, 3, 4, 6, 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512, 768,
-        1024, 1536, 2048, 3072, 4096, 6144, 8192, 12288, 16384, 24576, 32768, 49152
-                                              };
-const quint8 DistDirectBits[DistTableSize64] = {0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14};
+const quint8 LenDirectBits32[FixedLenTableSize] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    3, 3, 3, 3, 4, 4, 4, 4,
+    5, 5, 5, 5, 0, 0, 0
+};
+
+const quint8 LenDirectBits64[FixedLenTableSize] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    3, 3, 3, 3, 4, 4, 4, 4,
+    5, 5, 5, 5, 16, 0, 0
+};
+
+const quint32 DistStart[DistTableSize64]  = {
+    0, 1, 2, 3, 4, 6, 8, 12,
+    16, 24, 32, 48, 64, 96, 128, 192,
+    256, 384, 512, 768, 1024, 1536, 2048, 3072,
+    4096, 6144, 8192, 12288, 16384, 24576, 32768, 49152
+};
+
+const quint8 DistDirectBits[DistTableSize64] = {
+    0, 0, 0, 0, 1, 1, 2, 2,
+    3, 3, 4, 4, 5, 5, 6, 6,
+    7, 7, 8, 8, 9, 9, 10, 10,
+    11, 11, 12, 12, 13, 13, 14, 14
+};
 
 const quint8 LevelDirectBits[3] = {2, 3, 7};
 
-const quint8 CodeLengthAlphabetOrder[LevelTableSize] = {16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15};
+const quint8 CodeLengthAlphabetOrder[LevelTableSize] = {
+    16, 17, 18, 0, 8, 7, 9, 6,
+    10, 5, 11, 4, 12, 3, 13, 2,
+    14, 1, 15
+};
 
 enum {
     MatchMinLen = 3,
